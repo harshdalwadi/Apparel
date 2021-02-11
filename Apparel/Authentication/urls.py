c3874a040250cc1d26ddb8b3756c6e10310077a2
Harsh Dalwadi <dalwadi.harsh08@gmail.com>
2021-02-11 12:53:20 +0530

    Revert "commit"
    This reverts commit ddc4860b6c28c20654771f2f23ed70c15d91ea8d.

--- Apparel/Authentication/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('ajax/validate_username/signup', validate_username_signup, name='validate_username_signup'),
-   	path('ajax/validate_username/login/', validate_username_login, name='validate_username_login'),
-   	path('ajax/validate_username/password_reset/',validate_user_password_reset,name='validate_user_password_reset'),
-   	path('ajax/validate_user/password_change/', validate_user_password_change, name='validate_user_password_change'),
-]
--- Apparel/custom_admin/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from products.models import Category
-
-
-class CatForm(forms.ModelForm):
-    class Meta:
-        model = Category
-        fields = "__all__"
